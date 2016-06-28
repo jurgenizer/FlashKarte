4b4faa4c20281aed298d66344e99b9f2795a0be1
Jurgen <jurgen.geitner@gmail.com>
2016-06-28 12:20:40 +0200

    added info page and ui icon

---
+++ Flashcards/InfoViewController.swift
@@ -0,0 +1,35 @@
+//
+//  InfoViewController.swift
+//  FlashKarte
+//
+//  Created by Jurgen Geitner on 2016/06/28.
+//  Copyright © 2016 Jurgen Geitner. All rights reserved.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
